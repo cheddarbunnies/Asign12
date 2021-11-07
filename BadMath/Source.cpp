54f47467bd22e9bc9297ca8b05d1cd42c9baeb29
cheddarbunny <bunny@bunny.com>
2021-11-06 23:31:02 -0400

    Updated float average() line 6 in the parameter to list ints as int n1, n2, and n3. Updated float average() line 8 to return (n1 + n2 + n3) / 3.0f so that average was calculated succesfully with ( ). Updated int main() at line 18 to show n3 instead of n2 to store the 3rd number entered by the user. Added comments at line 5 and line 11.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,18 +2,20 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+//Calculate the average of the 3 numbers
+float average(int n1, int n2, int n3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (n1 + n2 + n3) / 3.0f;
 }
 
+//Main function, ask users for three numbers to find average & give result
 int main()
 {
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
